981a360daf2429fcea0c023ed952345a66b807e4
Michael Mayorov <mmajorov@gmail.com>
2016-05-05 18:42:25 +0300

    active scope for address object model

--- app/models/fias/address_object.rb
+++ app/models/fias/address_object.rb
@@ -4,14 +4,6 @@ module Fias
   class AddressObject < ActiveRecord::Base
     include Fias::Concerns::Uploadable
     #include Fias::Concerns::Updatable
-    #default_scope { where(actstatus: '1', livestatus: '1') }
-
-    #scope :child_of, ->(parent) { where(parentguid: parent) }
-    #scope :level, ->(level) { where(aolevel: level) }
-    #scope :regions, -> { where(parentguid: nil) }
-    #scope :areas, -> { where('citycode = ? AND streetcode = ? AND ctarcode = ? AND placecode = ?', '000', '0000', '000', '000') }
-    #scope :cities, -> { where("citycode <> '000' or streetcode <> '0000' ") }
-    #scope :streets, -> { where("streetcode <> '0000'") }
 
     scope :child_of, ->(parent) { where(parentguid: parent) }
     scope :level, ->(level) { where(aolevel: level) }
@@ -20,6 +12,7 @@ module Fias
     scope :cities, -> { level(4) }
     scope :towns, -> { level([5, 6]) }
     scope :streets, -> { level(7) }
+    scope :active, ->{ where(actstatus: '1', livestatus: '1') }
 
     def self.batch_update(attrs)
       created = 0
